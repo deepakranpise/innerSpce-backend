51b31c663423d27f9e743bd329ecad6b43b7a237
Deepak Mane <manedeep2001@gmail.com>
2023-06-28 19:43:29 +0530

    changes in db

--- classes/db.js
+++ classes/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 function connectDB() {
     try {
         mongoose.set('strictQuery', false)
-        mongoose.connect(process.env.dbUrl,
+        mongoose.connect(process.env.localDb,
             {
                 useNewUrlParser: true,
                 useUnifiedTopology: true,
